009674cc835b6f505d605e5a9d688c86112ed338
arthurL-daSilva <arthurleal723@gmail.com>
2024-12-05 20:17:10 -0300

    Feito alterações pontuais no css

--- src/pages/styles.jsx
+++ src/pages/styles.jsx
@@ -4,12 +4,15 @@ export const Container = styled.div `
     background: #b752ff;
     padding: 20px;
     border-radius: 10px;
+    opacity: 80%;
     
-
     h1 {
-        margin-left: 10px;
+        padding-left: 20px;
         color: #000;
         animation: pop_h1 0.5s;
+        font-family: "Roboto Condensed", sans-serif;
+        font-weight: 700;
+
     }
 
     hr {
@@ -50,17 +53,24 @@ export const Container = styled.div `
 `
 
 export const AddButton = styled.button `
-    background: #6cf000;
+    background: #6495ED;
     color:#000;
     height: 35px;
+    opacity: 0.8;
     margin-left: 10px;
     border-radius: 5px;
     padding: 5px 10px;
     cursor: pointer;
     border: none;
+    transition: 0.3s;
+    
 
     &:hover {
-        opacity: 0.8;
+        opacity: 1;
+        border-color: #000;
+        color: white;
+        border: solid;
+        border-width: 1px
     }
 `
 
